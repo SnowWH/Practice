ea161d3461ffb11452a9fa72a86907f52a6816a0
SnowWH <57125406+SnowWH@users.noreply.github.com>
2019-10-31 14:56:29 +0800

    Update Delivery.java

--- Delivery.java
+++ Delivery.java
@@ -1,43 +1,19 @@
-import java.util.*;
 class Delivery
 {
 	public static void main(String[] args)
 	{
-		int c=3;
-		int m=4;
-		int b=5;
-		Goods starbuck=new Goods(c,m,b);
-		starbuck.price();
-	}
-}
-
-class Goods
-{
-	int cup;
-	int macha;
-	int bean;
-	double cb;
-	double mb;
-	double bb;
-	Goods(int c,int m,int b)
-	{
-		cup=c;
-		macha=m;
-		bean=b;
-		cb=cup/1000;
-		if(cup%1000 !=0)
-			cb+=0.5;
-		mb=macha/100;
-		if(macha%100 !=0)
-			mb+=0.5;
-		bb=bean/50;
-		if(bean%50 !=0)
-			bb+=0.5;
-	}
-	public void price()
-	{
-		System.out.printf("cup, %.1f box, %.2f Yuan \n",cb,cb*1000.0*0.05);
-		System.out.printf("pack, %.1f box, %.2f Yuan \n",bb,bb*100.0*10.05);
-		System.out.printf("pack, %.1f box, %.2f Yuan \n",cb,cb*50.0*58.00);
+		Goods starbuck=new Goods();
+		int []g=new int[3];
+		starbuck.setGoods();
+		g=starbuck.getGoods();
+		CupThread cup=new CupThread();
+		MachaThread macha=new MachaThread();
+		BeanThread bean= new BeanThread();
+		cup.setNumber(g[0]);
+		macha.setNumber(g[1]);
+		bean.setNumber(g[2]);
+		cup.start();
+		macha.start();
+		bean.start();
 	}
 }
